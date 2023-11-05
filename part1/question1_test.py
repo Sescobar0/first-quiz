2d2727b7bb9f7e01d5c66f82b6e5fe7dd88629c4
Sescobar0 <148839742+Sescobar0@users.noreply.github.com>
2023-11-04 21:08:43 -0500

    Update question1_test.py

--- part1/question1_test.py
+++ part1/question1_test.py
@@ -2,6 +2,8 @@ from question1 import get_city_weather
 
 def test_get_city_weather():
 
+  assert get_city_weather("Sao Paulo") == "17 degrees and cloudy"
+
   assert get_city_weather("Quito") == "22 degrees and sunny"
 
   assert get_city_weather("New York") == "14 degrees and rainy"
